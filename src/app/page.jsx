d5e88a4bf728c678e583f3d98a992c6fe42d3cd6
Mohammadshadain <shadain044714@gmail.com>
2024-08-07 01:14:41 +0530

    React Library directory

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import React,{Component} from 'react';
+import {header}'./compoents/header';
+
+const HOME = () => {
+  return (
+    <div>
+      <h1>
+      HOME PAGE
+      </h1>
+    </div>
+  )
+}
+
+export default HOME
