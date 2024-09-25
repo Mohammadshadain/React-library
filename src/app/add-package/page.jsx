3c9f23c0accbaa4826c8b00747ba30f948864e84
JasimDAce <mohdalijasim786@gmail.com>
2024-09-26 01:26:22 +0530

    k

--- src/app/add-package/page.jsx
+++ src/app/add-package/page.jsx
@@ -72,10 +72,15 @@ const Login = () => {
     <>
       <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
         <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-            <div className="mt-12 flex flex-col items-center">
-              <div className="w-full flex-1 mt-8">
-               
+          <div className="lg:w-1/2 xl:w-5/12 p-4 sm:p-12">
+            <div className="mt-2 flex flex-col items-center">
+              <div className="w-full flex-1 mt-2">
+                <div className="flex flex-col items-center">
+                  <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-green-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+
+                    <span className="ml-4  text-xl">ADD LIBRARY</span>
+                  </button>
+                </div>
                 <div className="my-12 border-b text-center">
                   <Link
                     href="./signup"
