7684a981e8744d4d3b6cac56c425ffcb3e95053e
Mohammad Shadain <shadain044714@gmail.com>
2026-04-23 11:39:02 +0530

    Fix HTML entity encoding in sign-in page

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -70,14 +70,14 @@ const Login = () => {
               Sign in
             </motion.h1>
             <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
-              Don't have an account yet?
-              <a
-                className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
-                href="/sign-up"
-              >
-                Sign up here
-              </a>
-            </p>
+  Don&apos;t have an account yet?
+  <a
+    className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
+    href="/sign-up"
+  >
+    Sign up here
+  </a>
+</p>
           </div>
           <div className="mt-5">
             <motion.button
