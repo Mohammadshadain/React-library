188dc6822d78c8143fd2b016418d1e4f8c0ad2db
Mohammad Shadain <shadain044714@gmail.com>
2026-04-24 11:10:46 +0530

    /

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -22,7 +22,7 @@ export default function MyModal() {
         Open dialog
       </Button>
 
-      <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close} __demoMode>
+      <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close}>
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
